7f94a8ea50d3f81e1a90ff701995f863ef982466
MetaMintauckyte <113545506+MetaMintauckyte@users.noreply.github.com>
2022-09-30 01:31:34 +0300

    Create mylib.hpp

---
+++ mylib.hpp
@@ -0,0 +1,13 @@
+#include<fstream>
+#include<iostream>
+#include<string>
+#include<iomanip>
+#include<bits/stdc++.h>
+
+using std::cout;    
+using std::cin;
+using std::endl;
+using namespace std;
+using std::string;
+using std::copy;
+using std::vector;
